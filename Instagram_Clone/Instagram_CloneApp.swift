4f96f29368d6afc2d65dfa9a37d55da7e182bb9a
jbass-101 <junior07bj@gmail.comm>
2023-11-03 19:02:24 +0200

    Initial Commit

---
+++ Instagram_Clone/Instagram_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Instagram_CloneApp.swift
+//  Instagram_Clone
+//
+//  Created by Paulette Stevena on 2023/11/03.
+//
+
+import SwiftUI
+
+@main
+struct Instagram_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
