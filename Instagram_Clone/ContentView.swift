6ab82e10113bcb4ef70fa7341a03b597f728f6a0
jbass-101 <junior07bj@gmail.comm>
2023-11-06 22:33:22 +0200

    Keeping track of User sessions

--- Instagram_Clone/ContentView.swift
+++ Instagram_Clone/ContentView.swift
@@ -8,14 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @StateObject var vm = ContentViewModel()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        Group{
+            if vm.userSession == nil {
+                LoginView()
+                
+            } else {
+                MainTabView()
+            }
         }
-        .padding()
     }
 }
 
---
+++ Instagram_Clone/ContentViewModel.swift
@@ -0,0 +1,33 @@
+//
+//  ContentViewViewModel.swift
+//  Instagram_Clone
+//
+//  Created by Paulette Stevena on 2023/11/06.
+//
+
+import Foundation
+import Firebase
+import Combine
+
+class ContentViewModel : ObservableObject {
+    
+    private let service = AuthService.shared
+    private var cancellables = Set<AnyCancellable>()
+    
+    @Published var userSession: FirebaseAuth.User?
+    
+    init(){
+        setupSubscribers()
+        
+    }
+    
+    //Listen to changes of the user session by using combine
+    func setupSubscribers(){
+        service.$userSession.sink { [weak self] userSession in
+            self?.userSession = userSession
+        }
+        .store(in: &cancellables)
+    }
+    
+    
+}
